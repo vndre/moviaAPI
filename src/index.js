d3fbfd2dfbf1fdcd0d3284d1c0db1ece7492be49
anskydev <dev@ansky.sh>
2019-08-15 15:19:51 -0600

    move index file to src folder

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import Koa from 'koa'
+import { ApolloServer } from 'apollo-server-koa'
+import { success } from 'signale'
+import './api/database'
+import schema from './api/graphql'
+
+const server = new ApolloServer(schema)
+const app = new Koa()
+
+server.applyMiddleware({ app })
+
+app.listen({ port: 4000 }, () => {
+  success(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+})
