4d1197afb86c019c6c1d1a1fa7124945fd27068e
anskydev <dev@ansky.sh>
2019-08-16 14:20:15 -0600

    update actor query resolvers

--- src/api/graphql/resolvers/Actor/queries.js
+++ src/api/graphql/resolvers/Actor/queries.js
@@ -1,10 +1,11 @@
 import Actor from '@models/actor.model'
 
-export const getActors = async (_, { limit = 50, name = '' }) => {
-  const retrievedActors = await Actor
+export const actors = async (_, { limit = 50, name = '' }) => {
+  const dbActors = await Actor
     .find(name ? { name } : {})
     .sort({ name: 1 })
     .limit(limit)
+    .populate('directors')
 
-  return retrievedActors
+  return dbActors
 }
