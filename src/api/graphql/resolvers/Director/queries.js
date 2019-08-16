a27bc04d290d56bb986466718e05095d925083c8
anskydev <dev@ansky.sh>
2019-08-16 14:20:45 -0600

    update director query resolvers

--- src/api/graphql/resolvers/Director/queries.js
+++ src/api/graphql/resolvers/Director/queries.js
@@ -1,10 +1,10 @@
 import Director from '@models/director.model'
 
-export const getDirectors = async (_, { limit = 50, name = '' }) => {
-  const retrievedActors = await Director
+export const directors = async (_, { limit = 50, name = '' }) => {
+  const dbDirectors = await Director
     .find(name ? { name } : {})
     .sort({ name: 1 })
     .limit(limit)
 
-  return retrievedActors
+  return dbDirectors
 }
