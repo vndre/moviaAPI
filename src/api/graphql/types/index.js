2f7b3cf869edcf554a472158c70fb8cade129398
anskydev <dev@ansky.sh>
2019-08-16 14:21:33 -0600

    merge GQL types

--- src/api/graphql/types/index.js
+++ src/api/graphql/types/index.js
@@ -1,5 +1,6 @@
+import { mergeTypes } from 'merge-graphql-schemas'
 import Movie from './Movie/movie.schema'
 import Actor from './Actor/actor.schema'
 import Director from './Director/director.schema'
 
-export default [Movie, Actor, Director]
+export default mergeTypes([Movie, Actor, Director], { all: true })
