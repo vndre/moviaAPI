199c182be1cc501650fae62d2d6ef03d7c5a2337
anskydev <dev@ansky.sh>
2019-08-16 14:21:01 -0600

    update movie query resolvers

--- src/api/graphql/resolvers/Movie/queries.js
+++ src/api/graphql/resolvers/Movie/queries.js
@@ -1,10 +1,10 @@
 import Movie from '@models/movie.model'
 
-export const getMovies = async (_, { limit = 50, title = '' }) => {
-  const retrievedMovies = await Movie
+export const movies = async (_, { limit = 50, title = '' }) => {
+  const dbMovies = await Movie
     .find(title ? { title } : {})
     .sort({ title: 1 })
     .limit(limit)
 
-  return retrievedMovies
+  return dbMovies
 }
