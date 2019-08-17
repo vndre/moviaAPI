524ef1da06d7ac3a7b686ace7674df75d4a49afe
anskydev <dev@ansky.sh>
2019-08-17 13:28:49 -0600

    add JWT logic to server

--- src/index.js
+++ src/index.js
@@ -1,14 +1,63 @@
 import Koa from 'koa'
-import { ApolloServer } from 'apollo-server-koa'
-import { success } from 'signale'
+import jwt from 'koa-jwt'
+import { ApolloServer, gql } from 'apollo-server-koa'
+import { success, fatal } from 'signale'
+import { verify as JWTVerify } from 'jsonwebtoken'
+import { typeDefs, resolvers } from './api/graphql/schema'
 import './api/database'
-import schema from './api/graphql/schema'
 
-const server = new ApolloServer(schema)
+const { JWT_SECRET, NODE_ENV } = process.env
+const port = 4000
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  introspection: true,
+  playground: {
+    settings: {
+      'editor.theme': 'dark',
+    },
+    tabs: [
+      {
+        endpoint: NODE_ENV === 'production'
+          ? 'f'
+          : `http://localhost:${port}/graphql`,
+        name: 'movies',
+        query: '{\n  movies {\n    title \n    actors {\n      name\n    }\n  }\n}',
+      },
+    ],
+  },
+  context: async ({ ctx }) => {
+    let isLogged = false
+    if (typeof ctx.request.headers.authorization !== 'undefined') {
+      console.log('uh oh')
+      await JWTVerify(
+        ctx.request.headers.authorization,
+        JWT_SECRET,
+        (err, decoded) => {
+          if (err) fatal(err)
+          if (decoded) isLogged = true
+        },
+      )
+    }
+    return { isLogged }
+  },
+})
+
 const app = new Koa()
+
+app.use(jwt({ secret: JWT_SECRET }).unless({ path: server.graphqlPath }))
 
 server.applyMiddleware({ app })
 
+console.log(gql`
+  {
+    movies {
+      title
+    }
+  }
+`)
+
 app.listen({ port: 4000 }, () => {
-  success(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`)
+  success(`🚀 Server ready at http://localhost:${port}${server.graphqlPath}`)
 })
